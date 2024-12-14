2f80b99969c9a82813c53403922d044ce63a21f5
basharul2002 <basharulalam6@gmail.com>
2024-12-14 21:45:05 +0600

    Updated home.php(Home Slider Added)

---
+++ components/footer.php
@@ -0,0 +1,61 @@
+<div class = "top-footer">
+    <h2><i class = "bx bx-envelope"></i>Sign Up For Newslatter</h2>
+    <div class = "input-field">
+        <input type = "text" name = "" placeholder = "Enter your email">
+        <button class = "btn"> Subscribe</button>
+    </div>
+</div>
+<footer>
+    <div class = "overlay"></div>
+    <div class = "footer-content">
+        <div class = "img-box">
+            <img src = "assets/image/logo2.png">
+        </div>
+        <div class = "inner-footer">
+            <div class = "card"> 
+                <h3>about us</h3>
+                <ul>
+                    <li>about us</li>
+                    <li>our difference</li>
+                    <li>community matters</li>
+                    <li>press</li>
+                    <li>blog</li>
+                    <li>bouqs video</li>
+                </ul>
+            </div>
+            <div class = "card"> 
+                <h3>services</h3>
+                <ul>
+                    <li>order</li>
+                    <li>help centre</li>
+                    <li>shipping</li>
+                    <li>trem of use</li>
+                    <li>account details</li>
+                    <li>my account</li>
+                </ul>
+            </div>
+            <div class = "card"> 
+                <h3>local</h3>
+                <ul>
+                    <li>Kuratoli</li>
+                    <li>Kamalapur</li>
+                    <li>Kawla</li>
+                    <li>Mipur Sare 11</li>
+                </ul>
+            </div>
+            <div class = "card"> 
+                <h3>Newslattter</h3>
+                <p>Sign Up FOr Newslatter</p>
+                <div class = "social-links"> 
+                    <i class = "bx bxl-instagram"></i>
+                    <i class = "bx bxl-twitter"></i>
+                    <i class = "bx bxl-youtube"></i>
+                    <i class = "bx bxl-whatsapp"></i>
+                </div>
+            </div>
+        </div>
+        <div class = "bottom-footer"> 
+            <p>all right reserved</p>
+        </div>
+    </div>
+</footer>
--- home.php
+++ home.php
@@ -17,6 +17,69 @@
 </head>
 <body>
     <?php include 'components/header.php'; ?>
+    <div class = "main">
+        <?php include 'components/footer.php'; ?>
+        <div class = "home-section">
+           <div class = "slide__slider slide1"> 
+            <div class = "overlay"></div>
+                <div class = "slide-detail">
+                    <h1></h1>
+                    <p>Basharul - Alam - Mazu</p>
+                    <a href = "view_products.php" class = "btn">shop now</a>
+                </div>
+                <div class = "hero-dec-top"></div>
+                <div class = "hero-dec-bottom"></div>
+           </div>
+           <!--end-->
+           <div class = "slide__slider slide2"> 
+            <div class = "overlay"></div>
+                <div class = "slide-detail">
+                    <h1></h1>
+                    <p>Welcome to shop/p>
+                    <a href = "view_products.php" class = "btn">shop now</a>
+                </div>
+                <div class = "hero-dec-top"></div>
+                <div class = "hero-dec-bottom"></div>
+           </div>
+           <div class = "slide__slider slide3"> 
+            <div class = "overlay"></div>
+                <div class = "slide-detail">
+                    <h1></h1>
+                    <p>Basharul - Alam - Mazu</p>
+                    <a href = "view_products.php" class = "btn">shop now</a>
+                </div>
+                <div class = "hero-dec-top"></div>
+                <div class = "hero-dec-bottom"></div>
+           </div>
+           <!--end-->
+           <div class = "slide__slider slide4"> 
+            <div class = "overlay"></div>
+                <div class = "slide-detail">
+                    <h1></h1>
+                    <p>Basharul - Alam - Mazu</p>
+                    <a href = "view_products.php" class = "btn">shop now</a>
+                </div>
+                <div class = "hero-dec-top"></div>
+                <div class = "hero-dec-bottom"></div>
+           </div>
+           <!--end-->
+           <div class = "slide__slider slide5"> 
+            <div class = "overlay"></div>
+                <div class = "slide-detail">
+                    <h1></h1>
+                    <p>Basharul - Alam - Mazu</p>
+                    <a href = "view_products.php" class = "btn">shop now</a>
+                </div>
+                <div class = "hero-dec-top"></div>
+                <div class = "hero-dec-bottom"></div>
+           </div>
+           <!--end-->
+           <div class = "left-arrow"><i class = "bx bxs-left-arrow"></i></div>
+           <div class = "left-arrow"><i class = "bx bxs-right-arrow"></i></div>
+
+        </div>
+        <!--Home Slider end -->
+    </div>
     <script src = "https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalerts.min.js"></script>
     <script src = "script.js"></script>
     <?php include 'components/alert.php'; ?>

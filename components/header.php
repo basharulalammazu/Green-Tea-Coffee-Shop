33d71cc9818786d91404c44f2c207f395f74ea2f
basharul2002 <basharulalam6@gmail.com>
2024-12-14 22:26:31 +0600

    Updated header.php

--- components/header.php
+++ components/header.php
@@ -1,6 +1,6 @@
 <header class = "header">
     <div class = "flex">
-        <a herf = "home.php" class = "logo"> <img src = "assets/image/logo.php"></a>
+        <a herf = "home.php" class = "logo"> <img src = "assets/image/logo.jpg"></a>
         <nav class = "navbar">
             <a href = "home.php">Home</a>
             <a href = "view_product.php">Product</a>
@@ -11,7 +11,7 @@
             <i class = "bx bxs-user" id = "user-btn"></i>
             <a href = "wishlist.php" class = "cart-btn"><i class = "bx bx-heart"></i><sup>0</sup></a>
             <a href = "cart.php" class = "cart-btn"><i class = "bx bx-cart-download"></i><sup>0</sup></a>
-            <i class = "bx bx-list-plus" id = "menu-btn" style "font-size: 2rem;"></i>
+            <i class = "bx bx-list-plus" id = "menu-btn" style = "font-size: 2rem;"></i>
         </div>
         <div class = "user-box"> 
             <p>User Name: <span><?php // echo $_SESSION['user_name']; ?></span></p>

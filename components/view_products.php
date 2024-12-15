413844ad30cdcc9634cb2fe094857b03c7beedac
basharul2002 <basharulalam6@gmail.com>
2024-12-15 19:41:56 +0600

    Update components/view_products.php {DB}

--- components/view_products.php
+++ components/view_products.php
@@ -1,3 +1,79 @@
+<?php
+    include 'components/connection.php';
+
+    session_start();
+    if (isset($_SESSION['user_id']))
+        $user_id = $_SESSION['user_id'];
+    else 
+        $user_id = '';
+
+    if (isset($_POST['logout']))
+    {
+        session_unset();
+        session_destroy();
+        header("location: login.php")
+    }
+
+    // Adding product in wishlist
+    if(isset($_POST['add_to_wishlist']))
+    {
+        $id = unique_id();
+        $product_id = $_POST['product_id'];
+
+        $verify_wishlist = $conn->prepare("SELECT * FROM `Wishlist` WHERE `User ID` = ? AND `Product ID` = ?");
+        $verify_wishlist -> execute([$user_id, $product_id]);
+
+        $cart_num = $conn->prepare("SELECT * FROM `Cart` WHERE `User ID` = ? AND `Product ID` = ?");
+        $cart_num -> execute([$user_id, $product_id]);
+
+        if ($verify_wishlist->rowCount() > 0)
+            $warning_mes[] = 'Producct already exist in you wishlist';
+        else if ($cart_num  -> rowCount() > 0)
+            $warning_mes[] = 'product already exist in your cart';
+        else 
+        {
+            $select_price = $_conn->prepare("SELECT * FROM 'Products' WHERE ID = ? LIMIT 1")
+            $select_price -> execute([$product_id]);
+            $fetch_price = $select_price -> fetch(PDO::FETCH_ASSOC);
+
+            $insert_wishlist = $conn->prepare("INSERT INTO `Wishlist` (`ID`, `User ID`, `Product ID`, `Price`) VALUES (?, ?, ?, ?)");
+            $insert_wishlist -> execute([$id, $user_id, $product_id, $fetch_price['price']]);
+            $success_mess[] = 'product added to wishlist successfully';
+        }
+    }
+
+     // Adding product in cart
+     if(isset($_POST['add_to_cart']))
+     {
+         $id = unique_id();
+         $product_id = $_POST['product_id'];
+
+         $qty = $_POST['qty']
+         $qty = filter_var($qty, FILTER_SANITIZE_STRING);
+
+         $verify_cart = $conn->prepare("SELECT * FROM `Cart` WHERE `User ID` = ? AND `Product ID` = ?");
+         $verify_cart -> execute([$user_id, $product_id]);
+ 
+         $max_cart_items = $conn->prepare("SELECT * FROM `Cart` WHERE `User ID` = ?");
+         $max_cart_items -> execute([$user_id]);
+ 
+         if ($verify_cart->rowCount() > 0)
+             $warning_mes[] = 'Producct already exist in you wishlist';
+         else if ($max_cart_items -> rowCount() > 20)
+             $warning_mes[] = 'Cart is full';
+         else 
+         {
+             $select_price = $_conn->prepare("SELECT * FROM 'Products' WHERE ID = ? LIMIT 1")
+             $select_price -> execute([$product_id]);
+             $fetch_price = $select_price -> fetch(PDO::FETCH_ASSOC);
+ 
+             $insert_cart = $conn->prepare("INSERT INTO `Cart` (`ID`, `User ID`, `Product ID`, `Price`, `Quantity`) VALUES (?, ?, ?, ?, ?)");
+             $insert_cart -> execute([$id, $user_id, $product_id, $fetch_price['price']], $qty);
+             $success_mess[] = 'product added to wishlist successfully';
+         }
+     }
+?>
+
 <style type = "text/css">
     <?php include 'style.css' ?>
 </style>

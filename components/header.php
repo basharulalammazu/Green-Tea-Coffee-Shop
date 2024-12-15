a1d0b0529e477d0e0ceb1fd11ec4bb1a7957f89f
basharul2002 <basharulalam6@gmail.com>
2024-12-15 20:19:19 +0600

    Update components/header.php {Comment cart and wishlist db code}

--- components/header.php
+++ components/header.php
@@ -1,6 +1,11 @@
 <header class = "header">
     <div class = "flex">
-        <a herf = "home.php" class = "logo"> <img src = "assets/image/logo.jpg"></a>
+        <!-- Logo Section -->
+        <a href = "home.php" class = "logo"> 
+            <img src = "assets/image/logo.jpg">
+        </a>
+
+        <!-- Navigation Bar -->
         <nav class = "navbar">
             <a href = "home.php">Home</a>
             <a href = "view_product.php">Product</a>
@@ -8,30 +13,67 @@
             <a href = "about.php">About Us</a>
             <a href = "contact.php">Contact Us</a>
         </nav>
+
+        <!-- Icon Section -->
         <div class = "icons">
+            <!-- User Icon -->
             <i class = "bx bxs-user" id = "user-btn"></i>
+
             <?php 
-                $count_wishlist_items = $conn->prepare("SELECT * FROM 'Wishlist' WHERE `User ID` = ?")
+            /*
+                // Database Query to Count Wishlist Items
+                // Prepares a SQL query to count all rows in the 'Wishlist' table for the logged-in user
+                $count_wishlist_items = $conn->prepare("SELECT * FROM `Wishlist` WHERE `User ID` = ?");
+                
+                // Executes the query with the `User ID` passed as a parameter (from the session or logged-in user context)
                 $count_wishlist_items -> execute([$user_id]);
+                
+                // Fetches the number of rows returned by the query
                 $total_wishlist_items = $count_wishlist_items->rowCount();
+                */
             ?>
-            <a href = "wishlist.php" class = "cart-btn"><i class = "bx bx-heart"></i><sup style="color: #fff;"><?=$total_wishlist_items ?></sup></a>
+            <!-- Wishlist Icon with Total Count -->
+            <a href = "wishlist.php" class = "cart-btn">
+                <i class = "bx bx-heart"></i>
+                <sup style="color: #fff;"> 0 </sup>
+                <!-- <sup style="color: #fff;"><?php /* echo $total_wishlist_items ; */ ?></sup> -->
+            </a>
+
             <?php 
-                $count_cart_items = $conn->prepare("SELECT * FROM 'Cart' WHERE `User ID` = ?")
+            /*
+                // Database Query to Count Cart Items
+                // Prepares a SQL query to count all rows in the 'Cart' table for the logged-in user
+                $count_cart_items = $conn->prepare("SELECT * FROM `Cart` WHERE `User ID` = ?");
+                
+                // Executes the query with the `User ID` passed as a parameter (from the session or logged-in user context)
                 $count_cart_items -> execute([$user_id]);
+                
+                // Fetches the number of rows returned by the query
                 $total_cart_items = $count_cart_items->rowCount();
+            */
             ?>
-            <a href = "cart.php" class = "cart-btn"><i class = "bx bx-cart-download"></i><sup style="color: #fff;"><?=$total_cart_items ?></sup></a>
+            <!-- Cart Icon with Total Count -->
+            <a href = "cart.php" class = "cart-btn">
+                <i class = "bx bx-cart-download"></i>
+                <sup style="color: #fff;"> 0 </sup>
+                <!-- <sup style="color: #fff;"><?php  /* echo $total_cart_items; */ ?></sup> -->
+            </a>
+
+            <!-- Menu Icon -->
             <i class = "bx bx-list-plus" id = "menu-btn" style = "font-size: 2rem;"></i>
         </div>
+
+        <!-- User Information Box -->
         <div class = "user-box"> 
             <p>User Name: <span><?php echo $_SESSION['user_name']; ?></span></p>
-            <p>Email: <span><?php echo $_SESSION['user_name']; ?></span></p>
+            <p>Email: <span><?php echo $_SESSION['user_email']; ?></span></p>
             <a href = "login.php" class = "btn">login</a>
             <a href = "registration.php" class = "btn">register</a>
+
+            <!-- Logout Form -->
             <form method = "post">
                 <button type = "submit" name = "logout" class = "logout">log out</button>
-        </form>
+            </form>
         </div>
     </div>
 </header>
---
+++ components/view_page.php
@@ -0,0 +1,112 @@
+<?php
+    include 'components/connection.php';
+
+    session_start();
+    if (isset($_SESSION['user_id']))
+        $user_id = $_SESSION['user_id'];
+    else 
+        $user_id = '';
+
+    if (isset($_POST['logout']))
+    {
+        session_unset();
+        session_destroy();
+        header("location: login.php")
+    }
+
+    // Adding product in wishlist
+    if(isset($_POST['add_to_wishlist']))
+    {
+        $id = unique_id();
+        $product_id = $_POST['product_id'];
+
+        $verify_wishlist = $conn->prepare("SELECT * FROM `Wishlist` WHERE `User ID` = ? AND `Product ID` = ?");
+        $verify_wishlist -> execute([$user_id, $product_id]);
+
+        $cart_num = $conn->prepare("SELECT * FROM `Cart` WHERE `User ID` = ? AND `Product ID` = ?");
+        $cart_num -> execute([$user_id, $product_id]);
+
+        if ($verify_wishlist->rowCount() > 0)
+            $warning_mes[] = 'Producct already exist in you wishlist';
+        else if ($cart_num  -> rowCount() > 0)
+            $warning_mes[] = 'product already exist in your cart';
+        else 
+        {
+            $select_price = $_conn->prepare("SELECT * FROM 'Products' WHERE ID = ? LIMIT 1")
+            $select_price -> execute([$product_id]);
+            $fetch_price = $select_price -> fetch(PDO::FETCH_ASSOC);
+
+            $insert_wishlist = $conn->prepare("INSERT INTO `Wishlist` (`ID`, `User ID`, `Product ID`, `Price`) VALUES (?, ?, ?, ?)");
+            $insert_wishlist -> execute([$id, $user_id, $product_id, $fetch_price['price']]);
+            $success_mess[] = 'product added to wishlist successfully';
+        }
+    }
+
+     // Adding product in cart
+     if(isset($_POST['add_to_cart']))
+     {
+         $id = unique_id();
+         $product_id = $_POST['product_id'];
+
+         $qty = $_POST['qty']
+         $qty = filter_var($qty, FILTER_SANITIZE_STRING);
+
+         $verify_cart = $conn->prepare("SELECT * FROM `Cart` WHERE `User ID` = ? AND `Product ID` = ?");
+         $verify_cart -> execute([$user_id, $product_id]);
+ 
+         $max_cart_items = $conn->prepare("SELECT * FROM `Cart` WHERE `User ID` = ?");
+         $max_cart_items -> execute([$user_id]);
+ 
+         if ($verify_cart->rowCount() > 0)
+             $warning_mes[] = 'Producct already exist in you wishlist';
+         else if ($max_cart_items -> rowCount() > 20)
+             $warning_mes[] = 'Cart is full';
+         else 
+         {
+             $select_price = $_conn->prepare("SELECT * FROM 'Products' WHERE ID = ? LIMIT 1")
+             $select_price -> execute([$product_id]);
+             $fetch_price = $select_price -> fetch(PDO::FETCH_ASSOC);
+ 
+             $insert_cart = $conn->prepare("INSERT INTO `Cart` (`ID`, `User ID`, `Product ID`, `Price`, `Quantity`) VALUES (?, ?, ?, ?, ?)");
+             $insert_cart -> execute([$id, $user_id, $product_id, $fetch_price['price']], $qty);
+             $success_mess[] = 'product added to wishlist successfully';
+         }
+     }
+?>
+
+<style type = "text/css">
+    <?php include 'style.css' ?>
+</style>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href = 'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel = 'stylesheet'>
+    <title>Green Coffee - Product Details Page</title>
+</head>
+<body>
+    <?php include 'components/header.php'; ?>
+    <div class = "main">
+    <div class = "banner">
+            <h1>Product Details</h1>
+        </div>
+        <div class = "title2">
+            <a href = "home.php">home</a><span>Product Details</span>
+        </div>
+    </div>
+    <section class = "view_page">
+        
+    </section>
+    
+    <?php include 'components/footer.php'; ?>
+    <!--Home Slider end-->
+        
+
+    </div>
+    <script src = "https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalerts.min.js"></script>
+    <script src = "script.js"></script>
+    <?php include 'components/alert.php'; ?>
+</body>
+</html>

347883e86905227c1d69ee495e7e8c07de7c9b14
basharul2002 <basharulalam6@gmail.com>
2024-12-15 15:28:42 +0600

    Add registration.php

---
+++ registration.php
@@ -0,0 +1,45 @@
+<style type = "text/css">
+    <?php include 'style.css';?>
+</style>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Green Tea - Register</title>
+</head>
+<body>
+    <div class = "form-container">
+        <section class = "form-container">
+            <div class = "title">
+                <img src = "assets/image/download.png">
+                <h1>Register Now</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis quo voluptatum repellat voluptatibus reprehenderit, ipsam, voluptates, rem eum soluta veniam exercitationem perferendis placeat illo quidem ducimus distinctio 
+                    voluptatem odit! Quibusdam.
+                </p>
+            </div>
+            <form action = "#" method = "post">
+                <div class = "input-field">
+                    <p>Your Name<sup>*</sup></p>
+                    <input type = "text" name = "name" placeholder = "Enter your name" maxlength = "50" required>
+                </div>
+                <div class = "input-field">
+                    <p>Email<sup>*</sup></p>
+                    <input type = "text" name = "email" placeholder = "Enter your email" maxlength = "50" oninput = "this.value = this.value.replace(/\s/g,'')" required >
+                </div>
+                <div class = "input-field">
+                    <p>Password<sup>*</sup></p>
+                    <input type = "password" name = "pass" placeholder = "Enter the password" maxlength = "50" oninput = "this.value = this.value.replace(/\s/g,'')" required >
+                </div>
+                <div class = "input-field">
+                    <p>Confirm Name<sup>*</sup></p>
+                    <input type = "password" name = "cpass" placeholder = "Enter password one more time" maxlength = "50" required>
+                </div>
+                <input type = "submit" name = "submit" value = "registration" class = "btn">
+                <p>Already have an account? <u><a href = "login.php">Login Now</a></u></p>
+            </form>
+        </section>
+    </div>
+</body>
+</html>

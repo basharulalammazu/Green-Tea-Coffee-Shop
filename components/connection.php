21ddfd5a4fc0e886699863a52ee45fe223ab59aa
basharul2002 <basharulalam6@gmail.com>
2024-12-15 23:55:13 +0600

    Unique_id function added on components/connection.php

--- components/connection.php
+++ components/connection.php
@@ -7,5 +7,17 @@
         return;
     }
 
+
+    function unique_id()
+    {
+        $chars = "0123456789abcdefghijklmnopqrstuwvxyzABCDEFGHIJKMNLOPQRSTUWVXYZ"
+        $charLength = strlen($chars);
+        $randmonString = '';
+
+        for ($i = 0; $i < 20; $i++)
+            $randmonString = $chars[mt_rand(0, $charLength-1)];
+
+        return $randmonString;
+    }
     
 ?>

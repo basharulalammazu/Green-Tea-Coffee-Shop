d250a062cc743c301c0f9b26d3c4c76051e0bcfc
tanjimmm <tanjimpromise252000@gmail.com>
2024-12-15 23:01:20 +0600

    New php added

--- registration.php
+++ registration.php
@@ -6,21 +6,48 @@ session_start();
 if (isset($_SESSION['user_id'])) {
     $user_id = $_SESSION['user_id'];
 } else {
-    $user_id = "";
+    $user_id = '';
 }
 
 //register user
 if (isset($_POST['submit'])) {
-    $id = uniqid();
+    //$id = unique_id(); //Function is to be created
     $name = $_POST['name'];
     $name = filter_var($name, FILTER_SANITIZE_STRING);
     $email = $_POST['email'];
     $email = filter_var($email, FILTER_SANITIZE_STRING);
     $pass = $_POST['pass'];
     $pass = filter_var($pass, FILTER_SANITIZE_STRING);
-    $cpass = $_POST['pass'];
-    $pass = filter_var($pass, FILTER_SANITIZE_STRING);
+    $cpass = $_POST['cpass'];
+    $cpass = filter_var($cpass, FILTER_SANITIZE_STRING);
+
+    $select_user = $conn->prepare("SELECT * FROM users WHERE email = ?");
+    $select_user->execute([$email]);
+    $row = $select_user->fetch(PDO::FETCH_ASSOC);
+
+    if ($select_user->rowCount() > 0) {
+        $message[] = 'Email already exists';
+    } else {
+        if ($pass != $cpass) {
+        $message[] = 'Confirm your password';
+        } else {
+            // Insert user
+            $insert_user = $conn->prepare("INSERT INTO users (id, name, email, password) VALUES (?, ?, ?, ?)");
+            $insert_user->execute([$id, $name, $email, $pass]);
+
+            // Select user details after insertion
+            $select_user = $conn->prepare("SELECT * FROM users WHERE email = ? AND password = ?");
+            $select_user->execute([$email, $pass]);
+            $row = $select_user->fetch(PDO::FETCH_ASSOC);
+            if ($select_user->rowCount() > 0) {
+                $_SESSION['user_id'] = $row['id'];
+                $_SESSION['user_name'] = $row['name'];
+                $_SESSION['user_email'] = $row['email'];
+            }
+        }
+    }
 }
+
 ?>
 
 

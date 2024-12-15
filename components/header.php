71c117933ff8505b90c07f0daeebd340cd53ad3d
basharul2002 <basharulalam6@gmail.com>
2024-12-15 19:49:58 +0600

    Update components/header.php {cart and wishlist}

--- components/header.php
+++ components/header.php
@@ -10,13 +10,23 @@
         </nav>
         <div class = "icons">
             <i class = "bx bxs-user" id = "user-btn"></i>
-            <a href = "wishlist.php" class = "cart-btn"><i class = "bx bx-heart"></i><sup style="color: #fff;">0</sup></a>
-            <a href = "cart.php" class = "cart-btn"><i class = "bx bx-cart-download"></i><sup style="color: #fff;">0</sup></a>
+            <?php 
+                $count_wishlist_items = $conn->prepare("SELECT * FROM 'Wishlist' WHERE `User ID` = ?")
+                $count_wishlist_items -> execute([$user_id]);
+                $total_wishlist_items = $count_wishlist_items->rowCount();
+            ?>
+            <a href = "wishlist.php" class = "cart-btn"><i class = "bx bx-heart"></i><sup style="color: #fff;"><?=$total_wishlist_items ?></sup></a>
+            <?php 
+                $count_cart_items = $conn->prepare("SELECT * FROM 'Cart' WHERE `User ID` = ?")
+                $count_cart_items -> execute([$user_id]);
+                $total_cart_items = $count_cart_items->rowCount();
+            ?>
+            <a href = "cart.php" class = "cart-btn"><i class = "bx bx-cart-download"></i><sup style="color: #fff;"><?=$total_cart_items ?></sup></a>
             <i class = "bx bx-list-plus" id = "menu-btn" style = "font-size: 2rem;"></i>
         </div>
         <div class = "user-box"> 
-            <p>User Name: <span><?php // echo $_SESSION['user_name']; ?></span></p>
-            <p>Email: <span><?php // echo $_SESSION['user_name']; ?></span></p>
+            <p>User Name: <span><?php echo $_SESSION['user_name']; ?></span></p>
+            <p>Email: <span><?php echo $_SESSION['user_name']; ?></span></p>
             <a href = "login.php" class = "btn">login</a>
             <a href = "registration.php" class = "btn">register</a>
             <form method = "post">

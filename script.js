76acbdddea558e5edb610b102f91ee005a1fb60e
tanjimmm <tanjimpromise252000@gmail.com>
2024-12-15 17:58:07 +0600

    minor bug fixes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*------------------Menu bar slider----------------- */
+
 const header = document.querySelector('header');
 function fixedNavbar() {
     header.classList.toggle('scroll', window.pageYOffset > 0);
@@ -17,3 +19,60 @@ userBtn.addEventListener('click', function() {
     let userBox = document.querySelector('.user-box');
     userBox.classList.toggle('active');
 });
+
+/*--------------------home slider--------------------*/
+
+"use strict"
+const leftArrow = document.querySelector('.left-arrow .bxs-left-arrow'),
+    rightArrow = document.querySelector('.right-arrow.bxs-right-arrow'),
+    slider = document.querySelector('.slider');
+
+/*-----------------scroll to right-----------------------------*/
+
+function scrollRight(){
+    if (slider.scrollWidth - slider.clientWidth === slider.scrollLeft) {
+        slider.scrollTo({
+            left: 0,
+            behavior: "smooth"
+        });
+    }
+    else {
+        slider.scrollBy({
+            left: window.innerWidth,
+            behavior: "smooth"
+        })
+    }
+}
+
+/*-----------------scroll to left---------------------------*/
+
+function scrollLeft(){
+    slider.scrollBy({
+        left: -window.innerWidth,
+        behavior: "smooth"
+    })
+}
+let timerId = setInterval(scrollRight, 7000);
+
+/*----------------reset timer to scroll right--------------*/
+
+function resetTimer() {
+    clearInterval(timerId);
+    timerId = setInterval(scrollRight, 7000);
+  }
+  
+  //--------------- scroll event---------------------------//
+
+  slider.addEventListener('click', function(ev) {
+    if (ev.target === leftArrow) {
+      scrollLeft();
+      resetTimer();
+    }
+  })
+  
+  slider.addEventListener('click', function(ev) {
+    if (ev.target === rightArrow) {
+      scrollRight();
+      resetTimer();
+    }
+  })

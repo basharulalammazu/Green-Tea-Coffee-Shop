689ee848836485a17f663762ebbaff750b5f20da
tanjimmm <tanjimpromise252000@gmail.com>
2024-12-16 20:55:23 +0600

    npm library changed to Swal.fire() script

--- admin/register.php
+++ admin/register.php
@@ -89,6 +89,7 @@ if (isset($_POST['register']))
         </section>
     </div>
 
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
     <script src="script.js" type="text/javascript"></script>
     <?php include '../components/alert.php';?>

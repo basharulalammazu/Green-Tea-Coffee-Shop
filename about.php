9871c5aece699a58b01b66ca4e50e331ba2c7e08
basharul2002 <basharulalam6@gmail.com>
2024-12-22 14:50:34 +0600

    Minor bug fixed

--- about.php
+++ about.php
@@ -4,7 +4,7 @@
     session_start();
     if (!isset($_SESSION['user_id'])) 
         $user_id = '';
-    $user_id = $_SESSION['user_id']; 
+    else $user_id = $_SESSION['user_id']; 
         
     if (isset($_POST['logout'])) 
     {
